9677ae9d11173d767269cdca1d095c1e12031002
Wilson Forero <wilson_camilo@hotmail.com>
2026-07-29 23:49:03 -0500

    Remove deprecated load_launchjs

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -59,11 +59,6 @@ return {
         return vim.json.decode(json.json_strip_comments(str))
       end
 
-      -- Extends dap.configurations with entries read from .vscode/launch.json
-      if vim.fn.filereadable(".vscode/launch.json") then
-        vscode.load_launchjs()
-      end
-
       local function load_env_variables()
         local variables = {}
         for k, v in pairs(vim.fn.environ()) do
