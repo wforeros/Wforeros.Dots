3be17fddcb774fe89384f7b90a14bfbbe1d60997
Wilson Forero <wilson_camilo@hotmail.com>
2026-07-30 00:11:47 -0500

    Add diffview-plus

---
+++ nvim/lua/plugins/diffview-plus.lua
@@ -0,0 +1,12 @@
+return {
+  "dlyongemallo/diffview-plus.nvim",
+  version = "*",
+  -- optional: lazy-load on command
+  -- cmd = {
+  --     "DiffviewOpen",
+  --     "DiffviewToggle",
+  --     "DiffviewFileHistory",
+  --     "DiffviewDiffFiles",
+  --     "DiffviewLog",
+  -- },
+}
